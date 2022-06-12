e0fb828ee72850266824cd5c86153883eca43169
kayshaou <kaysha.ou@gmail.com>
2022-06-12 16:20:12 +0800

    first commt;

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# learnspringaspect
+# learn spring aspectJ & Spring AOP
+## my own pace project (yet again)
+running on port 8999
---
+++ src/main/java/com/example/learnspringaspect/LearnspringaspectApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringaspect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringaspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringaspectApplication.class, args);
+	}
+
+}
